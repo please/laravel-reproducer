f8bbfae06f9ab1e52715ed7631d3855f266582d1
Jérôme Tamarelle <jerome@tamarelle.net>
2023-11-15 17:48:34 +0100

    Can't reproduce issue #2679
    https://github.com/mongodb/laravel-mongodb/issues/2679

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,16 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use MongoDB\Laravel\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    protected $connection = 'mongodb';
 
     /**
      * The attributes that are mass assignable.
@@ -20,6 +21,11 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'phone',
+        'role',
+        'status',
+        'gender',
+        'avatar',
         'password',
     ];
 
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    $user  = \App\Models\User::create(
+        [
+            'name' => fake()->name,
+            'email' => fake()->email,
+            'phone' => fake()->phoneNumber,
+            'password' => \Illuminate\Support\Facades\Hash::make('secret'),
+        ],
+    );
+
+    return response()->json($user);
+});
